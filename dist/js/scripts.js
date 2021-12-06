d1ada5047a197ed6fda3de91407c58106f0ba72a
Ivan <ivan.frontcoder@gmail.com>
2021-12-06 18:46:47 +0500

    .

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -8,26 +8,39 @@ $(document).ready(function () {
 	}
 
 	// Селекты
-	if ($('.form-dc__select select').length) {
-		var _dropdown;
-		var settings = { autoReinitialise: true };
-		$('.form-dc__select select').styler({
-			selectPlaceholder: '',
-			selectVisibleOptions: 8,
-			onFormStyled: function () {
-				_dropdown = $('.jq-selectbox__dropdown');
-				_dropdown.find('ul').wrap('<div class="scroll-pane" />');
-			},
-			onSelectOpened: function () {
-				var _ul = $(this).find('.jq-selectbox__dropdown ul');
-				var height = _ul.height();
-				var _srollPane = _dropdown.find('.scroll-pane');
-				_srollPane.height(height);
-				_ul.css('max-height', 'none');
-				_srollPane.jScrollPane(settings);
-			}
-		});
+	function formStyleSelect() {
+		if ($('.form-dc__select select').length) {
+			var _dropdown;
+			var settings = { autoReinitialise: true };
+			$('.form-dc__select select').styler({
+				selectPlaceholder: '',
+				selectVisibleOptions: 8,
+				onFormStyled: function () {
+					_dropdown = $('.jq-selectbox__dropdown');
+					_dropdown.find('ul').wrap('<div class="scroll-pane" />');
+				},
+				onSelectOpened: function () {
+					var _ul = $(this).find('.jq-selectbox__dropdown ul');
+					var height = _ul.height();
+					var _srollPane = _dropdown.find('.scroll-pane');
+					_srollPane.height(height);
+					_ul.css('max-height', 'none');
+					_srollPane.jScrollPane(settings);
+				}
+			});
+		}
 	}
+	formStyleSelect();
+
+	// if ($('.form-dc__select select').length) {
+	// 	$('.form-dc__select select').select2({
+	// 		minimumResultsForSearch: Infinity,
+	// 	})
+	// 	.on("select2:open", function () {
+	// 		$('.select2-results__options').wrap('<div class="scrollbar-inner"></div>');
+	// 		$('.scrollbar-inner').scrollbar();
+	// 	});
+	// }
 
 	// Табы
 	function tabs(tabs) {
@@ -47,6 +60,8 @@ $(document).ready(function () {
 					}else {
 						return false;
 					}
+					$('.form-dc__select select').styler('destroy');
+					formStyleSelect();
 				});
 			});
 		}
